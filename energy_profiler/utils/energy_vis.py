f0e0d0ed277e0879527b6a588a24ae0ab3464b75
Jack Monas <jmonas@princeton.edu>
2024-03-05 03:14:29 -0500

    slurm

--- energy_profiler/utils/energy_vis.py
+++ energy_profiler/utils/energy_vis.py
@@ -58,7 +58,11 @@ def get_power(device: str = 'gpu', debug: bool = True):
         power_stdout = power_stdout.split('\n')
         
         for line in power_stdout:
-            if 'Draw' in line.split(): gpu_power = float(line.split()[-2])
+            if 'Draw' in line.split(): 
+                print("hm")
+                print(line.split())
+                print(line.split()[-2])
+                gpu_power = float(line.split()[-2])
 
         if debug: print(f'GPU Power: {gpu_power : 0.02f} W')
 
